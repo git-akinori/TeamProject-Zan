71a821c62ee7b222fc80cd2edf455033c5b52a50
Akinori Miura <ariant615@gmail.com>
2017-12-02 09:18:08 +0900

    Add Scripts

---
+++ Assets/Game/CsvReader.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+
+public class CsvReader : MonoBehaviour {
+
+ //   // 区切り記号の設定
+ //   public const char delim = '\t';
+
+	//public List<string[]> ReadCSV(string filePath)
+ //   {
+ //       var csvFile = Resources.Load(filePath) as TextAsset;
+
+ //       var data = new List<string[]>();
+ //       var sr = new StringReader(csvFile.text);
+
+ //       while(sr.Peek() > -1)
+ //       {
+ //           var line = sr.ReadLine();
+ //           data.Add(line.Split(delim));
+ //       }
+
+ //       return data;
+ //   }
+}
---
+++ Assets/Game/Enemy/EnemySpawner.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour {
+
+    [SerializeField]
+    GameObject enemy;
+
+    List<GameObject> enemyList = new List<GameObject>();
+
+	void Start () {
+		
+	}
+	
+	void Update () {
+		
+	}
+
+    // Spawn Enemy Function
+    void SpawnEnemy()
+    {
+        var obj = Instantiate(enemy, transform);
+        enemyList.Add(obj);
+    }
+}
